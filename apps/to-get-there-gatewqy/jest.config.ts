076384c822b399075beb3d65260e01a90a5effb9
孤独な 宇宙飛行士 <50376577+TheLonelyAstronaut@users.noreply.github.com>
2023-04-14 22:38:52 +0300

    remove typo

--- apps/to-get-there-gatewqy/jest.config.ts
+++ apps/to-get-there-gatewqy/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'to-get-there-gatewqy',
+  displayName: 'to-get-there-gateway',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/to-get-there-gatewqy',
+  coverageDirectory: '../../coverage/apps/to-get-there-gateway',
 };
